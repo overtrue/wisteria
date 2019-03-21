f815c92ac0c885d6fec7949b80d14cbe839f332a
overtrue <anzhengchao@gmail.com>
2019-03-22 00:21:52 +0800

    New logo

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -1,22 +1,24 @@
 <link rel="canonical" href="{{ $canonical }}" />
 <link rel="stylesheet" href="{{ asset('vendor/wisteria/css/app.css') }}">
-<link rel="apple-touch-icon" sizes="57x57" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-57x57.png') }}">
-<link rel="apple-touch-icon" sizes="60x60" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-60x60.png') }}">
-<link rel="apple-touch-icon" sizes="72x72" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-72x72.png') }}">
-<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-76x76.png') }}">
-<link rel="apple-touch-icon" sizes="114x114" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-114x114.png') }}">
-<link rel="apple-touch-icon" sizes="120x120" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-120x120.png') }}">
-<link rel="apple-touch-icon" sizes="144x144" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-144x144.png') }}">
-<link rel="apple-touch-icon" sizes="152x152" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-152x152.png') }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('vendor/wisteria/img/favicons/apple-icon-180x180.png') }}">
-<link rel="icon" type="image/png" sizes="192x192"  href="{{ asset('vendor/wisteria/img/favicons/android-icon-192x192.png') }}">
-<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('vendor/wisteria/img/favicons/favicon-32x32.png') }}">
-<link rel="icon" type="image/png" sizes="96x96" href="{{ asset('vendor/wisteria/img/favicons/favicon-96x96.png') }}">
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('vendor/wisteria/img/favicons/favicon-16x16.png') }}">
-<link rel="mask-icon" href="{{ asset('vendor/wisteria/img/favicons/apple-icon.png') }}" color="#9561E2">
-<link rel="manifest" href="{{ asset('vendor/wisteria/img/favicons/manifest.json') }}">
-<meta name="msapplication-TileColor" content="#9561E2">
 <meta name="msapplication-TileImage" content="{{ asset('vendor/wisteria/img/favicons/ms-icon-144x144.png') }}">
+<link rel="apple-touch-icon-precomposed" sizes="57x57" href="{{ asset('vendor/wisteria/img/favicons/pple-touch-icon-57x57.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="{{ asset('vendor/wisteria/img/favicons/apple-touch-icon-114x114.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="{{ asset('vendor/wisteria/img/favicons/apple-touch-icon-72x72.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href={{ asset('vendor/wisteria/img/favicons/apple-touch-icon-144x144.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="60x60" href="{{ asset('vendor/wisteria/img/favicons/apple-touch-icon-60x60.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="120x120" href="{{ asset('vendor/wisteria/img/favicons/apple-touch-icon-120x120.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="76x76" href="{{ asset('vendor/wisteria/img/favicons/apple-touch-icon-76x76.png') }}" />
+<link rel="apple-touch-icon-precomposed" sizes="152x152" href="{{ asset('vendor/wisteria/img/favicons/apple-touch-icon-152x152.png') }}" />
+<link rel="icon" type="image/png" href="favicon-196x196.png" sizes="196x196" />
+<link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96" />
+<link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
+<link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
+<link rel="icon" type="image/png" href="favicon-128.png" sizes="128x128" />
+<meta name="msapplication-TileImage" content="{{ asset('vendor/wisteria/img/favicons/mstile-144x144.png') }}" />
+<meta name="msapplication-square70x70logo" content="{{ asset('vendor/wisteria/img/favicons/mstile-70x70.png') }}" />
+<meta name="msapplication-square150x150logo" content="{{ asset('vendor/wisteria/img/favicons/mstile-150x150.png') }}" />
+<meta name="msapplication-wide310x150logo" content="{{ asset('vendor/wisteria/img/favicons/mstile-310x150.png') }}" />
+<meta name="msapplication-square310x310logo" content="{{ asset('vendor/wisteria/img/favicons/mstile-310x310.png') }}" />
 <meta name="theme-color" content="#9561E2">
 <meta name="apple-mobile-web-app-title" content="{{ config('app.name') }}">
 <meta name="application-name" content="{{ config('app.name') }}">
