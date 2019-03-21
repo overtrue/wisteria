daa1e9cd9476ae7b6cabce0a08a3d4c43079921e
overtrue <anzhengchao@gmail.com>
2019-03-22 07:32:14 +0800

    repo link

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="flex items-center justify-center mt-16 mb-24 text-grey text-xs">
-    Powered by Wisteria.
+    Powered by <a href="https://github.com/overtrue/wisteria">Wisteria</a>.
 </footer>
