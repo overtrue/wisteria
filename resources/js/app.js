54bb6fd1f31ee8d11e2b43d87715da482bd6ec22
overtrue <anzhengchao@gmail.com>
2019-03-25 13:39:44 +0800

    Dark mode #6 & Right nav links #9

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,74 +14,84 @@ require('prismjs/components/prism-php.js')
 require('prismjs/components/prism-sass.js')
 require('prismjs/components/prism-scss.js')
 
-const files = require.context('./', true, /\.vue$/i)
+const files = require.context('./components', true, /\.vue$/i)
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-const app = new Vue({
-  el: '#content',
-  mounted () {
+class Wisteria {
+  constructor() {
+    this.initVueInstances()
     this.reformatContent()
     this.replaceQuoteIcons()
     this.createSmoothSidebar()
     this.initDocSearch()
     this.activateCurrentSection()
-  },
-  methods: {
-    reformatContent () {
-      document.querySelectorAll('.markdown-body blockquote blockquote').forEach((blockquote) => {
-        blockquote.outerHTML = blockquote.innerHTML
-      })
-    },
-    createSmoothSidebar () {
-      new PerfectScrollbar('#nav', {
-        wheelSpeed: 2,
-        wheelPropagation: true,
-        minScrollbarLength: 20
-      })
-    },
-    activateCurrentSection () {
-      let nav = document.querySelector('#nav')
-      let current = document.querySelector('.docs-index ul li a[href="' + WITERIA_FULL_URL + '"]')
+  }
 
-      if (current) {
-        current.classList.add('is-active', 'px-2', 'py-1', 'block', '-ml-2', 'bg-gray-100', 'rounded')
-        current.parentElement.classList.add('is-active')
-      }
-      if (current.getBoundingClientRect().top >= window.screen.height * 0.4) {
-        nav.scrollTop = current.getBoundingClientRect().top - window.screen.height * 0.4
-      }
-    },
-    initDocSearch() {
-      docsearch({
-        // Your apiKey and indexName will be given to you once
-        // we create your config
-        apiKey: WISTERIA_DOCS_DOCSEARCH_API_KEY,
-        indexName: WISTERIA_DOCS_DOCSEARCH_INDEX_NAME,
-        // Replace inputSelector with a CSS selector
-        // matching your search input
-        inputSelector: '#search-input',
-        // Set debug to true if you want to inspect the dropdown
-        debug: false
-      })
-    },
-    replaceQuoteIcons () {
-      document.querySelectorAll('.markdown-body blockquote').forEach(function (blockquote) {
-        let match = blockquote.innerHTML.match(/\{(.*?)\}/)
-        if (!match) {
-          return
-        }
-        const icon = match[1]
+  initVueInstances() {
+    this.navbar = new Vue({el: '#navbar'})
+    this.content = new Vue({el: '#content'})
+    console.log('abc')
+  }
 
-        let icons = {
-          info: '<svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 13.33a7 7 0 1 1 6 0V16H7v-2.67zM7 17h6v1.5c0 .83-.67 1.5-1.5 1.5h-3A1.5 1.5 0 0 1 7 18.5V17zm2-5.1V14h2v-2.1a5 5 0 1 0-2 0z"></path></svg>',
-          warning: '<svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 5h2v6H9V5zm0 8h2v2H9v-2z"></path></svg>'
-        }
-        icons.note = icons.info
-        icons.tips = icons.warning
+  reformatContent () {
+    document.querySelectorAll('.markdown-body blockquote blockquote').forEach((blockquote) => {
+      blockquote.outerHTML = blockquote.innerHTML
+    })
+  }
 
-        blockquote.innerHTML = `<div class="flag"><span class="svg">${icons[icon]}</span></div><div>${blockquote.innerHTML.replace(/\{(.*?)\}/, '')}</div>`
-        blockquote.classList.add(icon)
-      })
+  createSmoothSidebar () {
+    new PerfectScrollbar('#nav', {
+      wheelSpeed: 2,
+      wheelPropagation: true,
+      minScrollbarLength: 20
+    })
+  }
+
+  activateCurrentSection () {
+    let nav = document.querySelector('#nav')
+    let current = document.querySelector('.docs-index ul li a[href="' + WITERIA_FULL_URL + '"]')
+
+    if (current) {
+      current.classList.add('is-active', 'px-2', 'py-1', 'block', '-ml-2', 'bg-gray-100', 'rounded')
+      current.parentElement.classList.add('is-active')
+    }
+    if (current.getBoundingClientRect().top >= window.screen.height * 0.4) {
+      nav.scrollTop = current.getBoundingClientRect().top - window.screen.height * 0.4
     }
   }
-})
+
+  initDocSearch() {
+    docsearch({
+      // Your apiKey and indexName will be given to you once
+      apiKey: WISTERIA_DOCS_DOCSEARCH_API_KEY,
+      indexName: WISTERIA_DOCS_DOCSEARCH_INDEX_NAME,
+      // Replace inputSelector with a CSS selector
+      // matching your search input
+      inputSelector: '#search-input',
+      // Set debug to true if you want to inspect the dropdown
+      debug: false
+    })
+  }
+
+  replaceQuoteIcons () {
+    document.querySelectorAll('.markdown-body blockquote').forEach(function (blockquote) {
+      let match = blockquote.innerHTML.match(/\{(.*?)\}/)
+      if (!match) {
+        return
+      }
+      const icon = match[1]
+
+      let icons = {
+        info: '<svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 13.33a7 7 0 1 1 6 0V16H7v-2.67zM7 17h6v1.5c0 .83-.67 1.5-1.5 1.5h-3A1.5 1.5 0 0 1 7 18.5V17zm2-5.1V14h2v-2.1a5 5 0 1 0-2 0z"></path></svg>',
+        warning: '<svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 5h2v6H9V5zm0 8h2v2H9v-2z"></path></svg>'
+      }
+      icons.note = icons.info
+      icons.tips = icons.warning
+
+      blockquote.innerHTML = `<div class="flag"><span class="svg">${icons[icon]}</span></div><div>${blockquote.innerHTML.replace(/\{(.*?)\}/, '')}</div>`
+      blockquote.classList.add(icon)
+    })
+  }
+}
+
+new Wisteria()
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="flex items-center bg-white h-16 z-50 pin-t sticky border-t-4 border-b">
     <div class="container mx-auto">
-        <div class="flex items-center justify-between px-6 lg:px-16">
+        <div id="navbar" class="flex items-center justify-between px-6 lg:px-16">
             <a href="{{ url('/') }}">
                 <img src="{{ asset(config('wisteria.ui.logo')) }}" alt="Logo" style="height: 30px" />
             </a>
@@ -11,9 +11,15 @@
                     @endforeach
                 </div>
             @endif
-            @if(config('wisteria.plugins.docsearch.api_key'))
-                @include('wisteria::plugins.docsearch')
-            @endif
+            <div class="flex items-center justify-end">
+                @if(config('wisteria.plugins.docsearch.api_key'))
+                    @include('wisteria::plugins.docsearch')
+                @endif
+                @if(config('wisteria.docs.repository.provider') === 'github')
+                <github-link url="{{ config('wisteria.docs.repository.url')  }}" class="ml-4"></github-link>
+                @endif
+                <dark-mode-switcher class="ml-4"></dark-mode-switcher>
+            </div>
             <div id="nav-open" class="text-gray-dark lg:hidden">
                 <svg role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="fill-current w-6 h-6">
                     <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
