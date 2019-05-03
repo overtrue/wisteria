a344763ed71d172613f07cf146361c05df11e299
overtrue <anzhengchao@gmail.com>
2019-05-03 13:05:28 +0800

    wip

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,19 +1,6 @@
 const mix = require('laravel-mix')
 const tailwindcss = require('tailwindcss')
-const purgecss = require('@fullhuman/postcss-purgecss')({
 
-  // Specify the paths to all of the template files in your project
-  content: [
-    './resources/**/*.html',
-    './resources/**/*.vue',
-    './resources/**/*.jsx',
-    './resources/views/**/*.php',
-    // etc.
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
 
 /*
  |--------------------------------------------------------------------------
@@ -31,10 +18,7 @@ mix.js('resources/js/app.js', 'public/js')
     .options({
       processCssUrls: false,
       postCss: [
-        tailwindcss('./tailwind.config.js'),
-        ...process.env.NODE_ENV === 'production'
-          ? [purgecss]
-          : []
+        tailwindcss('./tailwind.config.js')
       ],
     })
 
